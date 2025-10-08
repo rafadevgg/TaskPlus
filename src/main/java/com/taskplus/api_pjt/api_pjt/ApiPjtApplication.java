27c63979dbe05ddb963063b0988ece4950ef7eeb
rafadevgg <rafaelpc5671@gmail.com>
2025-10-08 09:54:45 -0300

    Fix: initial commit!

---
+++ src/main/java/com/taskplus/api_pjt/api_pjt/ApiPjtApplication.java
@@ -0,0 +1,13 @@
+package com.taskplus.api_pjt.api_pjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskplus/api_pjt/api_pjt/ApiPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskplus.api_pjt.api_pjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
