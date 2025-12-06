e8c25e7a7a8b2f02f36342297454ed9012136a68
rafadevgg <rafaelpc5671@gmail.com>
2025-12-06 10:21:49 -0300

    fix: Ajustes no securityConfig pÃara liberarção de todos os Cors

--- src/main/java/com/taskplus/api_pjt/api_pjt/config/SecurityConfig.java
+++ src/main/java/com/taskplus/api_pjt/api_pjt/config/SecurityConfig.java
@@ -45,9 +45,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
